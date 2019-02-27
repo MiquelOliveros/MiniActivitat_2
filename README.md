97069d3646e57dcabe2639c7205ef510d2714446
Miquel Oliveros <misalmiki@gmail.com>
2019-02-27 20:34:56 +0100

    Canviats strings i afegits hints

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Second MiniActivity of android
 * [CV link](https://cv.udl.cat/access/content/attachment/102025-1819/Activitats/9915843c-6ed1-40c1-85c5-f76aa25ecc11/MiniActv-1.pdf)
 
 ### Limit Date ###
-??/03/19
+5/03/19
 
 ### Description ###
 Create from an empty activity a simple Android App to manage the intents of a simple application.
--- app/src/main/java/com/example/miquel/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/miquel/myapplication2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.miquel.myapplication2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**EditText editText1;
+    EditText editText2;
+
+    String bye = editText1.getText().toString();
+    String nrep = editText2.getText().toString();
+    int num = Integer.parseInt(nrep);**/
 }
