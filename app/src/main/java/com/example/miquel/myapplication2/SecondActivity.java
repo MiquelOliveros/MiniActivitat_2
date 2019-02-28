10005df38f418a351d3af1d8e78c5fa899e73bc5
ar98nau <anr3@alumnes.udl.cat>
2019-02-28 15:36:39 +0100

    Canvi de pantalla
    Signed-off-by: ar98nau <anr3@alumnes.udl.cat>

---
+++ app/src/main/java/com/example/miquel/myapplication2/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.miquel.myapplication2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secundary_activity);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
