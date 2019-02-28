400728b020fd34972d7885ac11bd76bc1e28605a
ar98nau <anr3@alumnes.udl.cat>
2019-02-28 15:35:15 +0100

    Canvi de pantalla
    Signed-off-by: ar98nau <anr3@alumnes.udl.cat>

--- app/src/main/java/com/example/miquel/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/miquel/myapplication2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.miquel.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    /**EditText editText1;
-    EditText editText2;
-
-    String bye = editText1.getText().toString();
-    String nrep = editText2.getText().toString();
-    int num = Integer.parseInt(nrep);**/
 }
